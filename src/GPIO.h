bb30b8db2b1a2a31ec4ca6c60a8107ed0d0ff71c
Baron Leonardo <mohamedayman.fcis@zoho.com>
2017-03-09 22:26:26 +0200

    rename `HEX` type into `hex_t`

--- src/GPIO.h
+++ src/GPIO.h
@@ -5,7 +5,8 @@
 #include <stdbool.h>
 #include "IO.h"
 
-#define HEX uint32_t
+typedef unsigned long ulong;
+#define hex_t ulong
 
 enum GPIO_ISR_EDGE {
     Edge_FALLING,
@@ -23,11 +24,11 @@ typedef enum GPIO_ISR_LEVEL GPIO_ISR_LEVEL;
 
 // functions
 void GPIO_pinMode( PORT_PIN, PIN_MODES mode );
-void GPIO_pinsMode( PORTS port, HEX pins, PIN_MODES mode );
+void GPIO_pinsMode( PORTS port, hex_t pins, PIN_MODES mode );
 void GPIO_pinWrite( PORT_PIN, PIN_STATE state );
 PIN_STATE GPIO_pinRead( PORT_PIN );
-void GPIO_pinsWrite( PORTS port, HEX pins, HEX states );
-HEX GPIO_pinsRead( PORTS port, HEX pins );
+void GPIO_pinsWrite( PORTS port, hex_t pins, hex_t states );
+hex_t GPIO_pinsRead( PORTS port, hex_t pins );
 
 void GPIO_ISR_edges( PORT_PIN, GPIO_ISR_EDGE ISR_edge, uint8_t priority, bool enable, void(*run)() );
 void GPIO_ISR_levels( PORT_PIN, GPIO_ISR_LEVEL ISR_level, uint8_t priority, bool enable, void(*run)() );
