c9a640e5ef443406e76da9200461e1874c4a4a06
Baron Leonardo <mohamedayman.fcis@zoho.com>
2017-03-15 18:10:04 +0200

    `main.c` should be empty but the basic template

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
-#include "tm4c123gh6pm.h"
 #include "src/GPIO.h"
+#include "src/UART.h"
+#include "src/SPI.h"
 
 int main()
 {
-    GPIO_pinsMode( PORT_F, 0xE, OUT );
-//    GPIO_pinMode( F0, IN_PULLUP );
 
     while (1)
     {
-            GPIO_pinWrite( F1, HIGH );
-            GPIO_pinWrite( F2, HIGH );
-            GPIO_pinWrite( F3, HIGH );
+        
     }
+
     return 0;
 }
