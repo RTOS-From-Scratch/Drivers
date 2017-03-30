568cafe9a2d6ecc95c64959b0441bf2753203d8c
Baron Leonardo <mohamedayman.fcis@zoho.com>
2017-03-30 16:47:36 +0200

    rename some files

---
+++ src/inner/__IO.c
@@ -0,0 +1,28 @@
+#include "__IO.h"
+#include "tm4c123gh6pm.h"
+
+#define PORTS_NUM 6
+
+static byte __IO_pinsUsed[PORTS_NUM] = {0};
+
+void __IO_setPinsBusy(PORT port, byte pins)
+{
+    __IO_pinsUsed[port] |= pins;
+}
+
+void __IO_setPinsFree(PORT port, byte pins)
+{
+    __IO_pinsUsed[port] &= ~pins;
+    // disable GPIO CLK
+    // free port close its clock
+    if( __IO_pinsUsed[port] == 0 )
+        SYSCTL_RCGCGPIO_R &= ~(1 << port);
+}
+
+bool __IO_isPinsAvailable(PORT port, byte pins)
+{
+    if( (__IO_pinsUsed[port] & pins) is 0 )
+        return true;
+    else
+        return false;
+}
