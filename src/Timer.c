50000078b49d2eda989fa8a4d282629590a1b9a7
sara <saraadel.fcis2012@gmail.com>
2017-05-30 14:48:02 +0200

    remove some unused code lines and modify the design

--- src/Timer.c
+++ src/Timer.c
@@ -2,60 +2,51 @@
 #include "inner/__IO.h"
 #include "tm4c123gh6pm.h"
 
-#define __TIMER_MODULE_NUMBER(timer_module) (byte)timer_module
 #define TIMER0 0
 #define SYSTIMER_MAX_TICKS 0xFFFFFFFF
 
 void Timer_init(TIMER_MODULE_t timer_module, __Timer_Mode mode, __Timer_Count_Dir dir, uint32_t value)
 {
-    byte timer_num = __TIMER_MODULE_NUMBER(timer_module);
-
-    //configure Timer clock register and set the bit alternated to the timer number
     // enable timer clk
-    uint32_t delay;
-    IO_REG(RCGC_BASE, RCGCTIMER_OFFSET) |= ( 1 << timer_num );
-    delay = IO_REG(RCGC_BASE, RCGCTIMER_OFFSET);
+    IO_REG(RCGC_BASE, RCGCTIMER_OFFSET) |= ( 1 << timer_module );
+    while((SYSCTL_PPTIMER_R & (1 << timer_module)) == 0);
 
     //disable TnEN bit in TIMERCTL register
-    IO_REG(__Timer_Addr[timer_num], TIMER_CTL_R) = 0x0;
+    IO_REG(__Timer_Addr[timer_module], TIMER_CTL_R) = 0x0;
 
     // clear configuration register
     // configure for 32-bit timer
-    IO_REG(__Timer_Addr[timer_num], TIMER_CFG_R) = 0x0;
+    IO_REG(__Timer_Addr[timer_module], TIMER_CFG_R) = 0x0;
 
     //if timer is periodic mode
     if(mode)
-        IO_REG(__Timer_Addr[timer_num], TIMER_TA_M_R) |= 0x2;
+        IO_REG(__Timer_Addr[timer_module], TIMER_TA_M_R) |= 0x2;
     //if timer is one shot mode
     else
-        IO_REG(__Timer_Addr[timer_num], TIMER_TA_M_R) = 0x1;
+        IO_REG(__Timer_Addr[timer_module], TIMER_TA_M_R) = 0x1;
 
     //set the direction of the counting
-    IO_REG(__Timer_Addr[timer_num], TIMER_TA_M_R)   |= (dir << 4);
+    IO_REG(__Timer_Addr[timer_module], TIMER_TA_M_R)   |= (dir << 4);
 
     //set the value of the timer
-    IO_REG(__Timer_Addr[timer_num], TIMER_TA_IL_R)  = value;
+    IO_REG(__Timer_Addr[timer_module], TIMER_TA_IL_R)  = value;
 }
 
 void Timer_start(TIMER_MODULE_t timer_module)
 {
-    byte timer_num = __TIMER_MODULE_NUMBER(timer_module);
-
-    IO_REG(__Timer_Addr[timer_num], TIMER_CTL_R) |= (1 << 0);
+    IO_REG(__Timer_Addr[timer_module], TIMER_CTL_R) |= (1 << 0);
 }
 
 bool Timer_isDone(TIMER_MODULE_t timer_module)
 {
-    byte timer_num = __TIMER_MODULE_NUMBER(timer_module);
-
-    while(!(IO_REG(__Timer_Addr[timer_num], TIMER_MRIS) & (1<< 0)));
+    while(!(IO_REG(__Timer_Addr[timer_module], TIMER_MRIS) & (1<< 0)));
 
     return true;
 }
 
 void Timer_SysTimer_init(int value)
 {
-    Timer_init(TIMER0_PORTB, __periodic_timer, __UP, 0xffffffff);
+    Timer_init(TIMER0, __periodic_timer, __UP, 0xffffffff);
 
     IO_REG(__Timer_Addr[TIMER0], TIMER_TA_M_R) |= 0x20;
     IO_REG(__Timer_Addr[TIMER0], TIMER_TA_MATCHR)   =  value;
@@ -67,7 +58,7 @@ void Timer_SysTimer_init(int value)
 void Timer_sysTimer_start()
 {
     // TODO: check if the timer initiated
-    Timer_start(TIMER0_PORTB);
+    Timer_start(TIMER0);
 }
 
 void Timer_sysTimer_stop()
